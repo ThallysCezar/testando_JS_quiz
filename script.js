ae2e35696cb9e98654a04ae40d51f7655e0cab4d
ThallysCezar <75546628+ThallysCezar@users.noreply.github.com>
2021-12-20 21:52:36 -0300

    Build: js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var age = document.getElementById('age');
-var height = document.getElementById('height');
-var weight = document.getElementById('weight');
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var female = document.getElementById("f");
+var male = document.getElementById("m");
+
+
+document.getElementById("submit").addEventListener("click", validadeForm)
+
+function validadeForm(){
+    if(age.value == '' || height.value == ''  || weight.value == '' || ()){
+
+    }
+}
